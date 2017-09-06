de6d29ac8eb730897a9f9690f2b0b6d3c01ca880
RakeYohnshair <31711821+RakeYohnshair@users.noreply.github.com>
2017-09-06 18:38:42 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int SecretNum, Guess;
+   SecretNum = 7;
+    cout << "Guess a number";
+    cin >> Geuss;
+    if (SecretNum = Guess)
+    then cout << "Correct";
+      return 0;
+}
