304009bcf2eda19534d86bbfb5c98e59d5af6fb2
RakeYohnshair <31711821+RakeYohnshair@users.noreply.github.com>
2017-09-06 18:40:32 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
   int SecretNum, Guess;
-   SecretNum = 7;
-    cout << "Guess a number";
-    cin >> Geuss;
-    if (SecretNum = Guess)
-    then cout << "Correct";
-      return 0;
+  SecretNum = 7;
+  cout << "Guess a number";
+  cin >> Geuss;
+  if (SecretNum = Guess)
+  then cout << "Correct";
+  return 0;
 }
