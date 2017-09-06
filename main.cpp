868580721ca786cdefd5138cad07335f1c07d999
RakeYohnshair <31711821+RakeYohnshair@users.noreply.github.com>
2017-09-06 18:49:11 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@ int main()
   int secretnum,
   int guess;
   secretnum = 7;
-  cout << "Guess a number";
+   cout << "Guess a number";
   cin >> guess;
-  if(secretnum == guess);
-  then cout << "Correct";
+  if(secretnum == guess) {
+    cout << "Correct";
+  }
   return 0;
 }
