9fc85210f40f6e0d5203ca5e06e40e8ab5a7a656
RakeYohnshair <31711821+RakeYohnshair@users.noreply.github.com>
2017-09-06 20:41:06 -0400

    Create (With Loop) main.cpp

---
+++ (With Loop) main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int secretnum = 7;
+  int guess;
+  while (secretnum != guess) 
+  {
+    cout << "Guess a number (1-10)";
+    cin >> guess;
+    if (secretnum != guess)
+  {
+    cout << "Wrong, try again" ;
+  }
+  }
+  return 0;
+}
